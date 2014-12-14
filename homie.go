9ddae76e027c5bab50ee4e3e6d468105d3252d4e
Michael May <m@mmay.rocks>
2014-12-14 11:40:21 -0800

    justgothings

---
+++ homie.go
@@ -0,0 +1,65 @@
+package homie
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func init() {
+	http.HandleFunc("/", base)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/blog", blog)
+	http.HandleFunc("/KF5JBH", arrl)
+}
+
+type Person struct {
+	Alias       string `json:"alias"`
+	Age         int    `json:"age"`
+	Gh          string `json:"gh"`
+	Slideshare  string `json:"slideshare"`
+	Speakerdeck string `json:"speakerdeck"`
+	Other       string `json:"other"`
+	Meta        Meta   `json:"meta"`
+}
+
+type Meta struct {
+	Geo      string    `json:"geo"`
+	Time     time.Time `json:"resp"`
+	UnixTime int64     `json:"req_unix_nano"`
+}
+
+func base(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Cache-Control", "public, maxage=30, s-maxage=60")
+
+	mm := Person{Alias: "mmay",
+		Age:         time.Now().Year() - 1990,
+		Gh:          "https://github.com/mmay",
+		Speakerdeck: "https://speakerdeck.com/mmay",
+		Slideshare:  "http://www.slideshare.net/iammichaelmay",
+		Meta: Meta{
+			Time:     time.Now(),
+			UnixTime: time.Now().Unix()}}
+
+	result, _ := json.MarshalIndent(mm, "", "  ")
+	io.WriteString(w, string(result))
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+
+}
+func blog(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func arrl(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func link(ref string, name string) string {
+	a := []string{"<a href=http://", ref, ">", name, "<\a>"}
+	return strings.Join(a, "")
+}
